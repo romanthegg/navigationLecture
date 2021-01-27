205acc9cb605e5582ef5c8ecb208d3d331e2403e
romanthegg <rzadorskyy3454@stu.d214.org>
2021-01-27 22:49:15 +0200

    Initial Commit

---
+++ navigationLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  navigationLecture
+//
+//  Created by Yanik Kukulnik on 27.01.2021.
+//  Copyright © 2021 Yanik Kukulnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
